cb2e0117755895d299670c9e6f37a079aaef5eeb
유빈 <bean02347@gmail.com>
2021-05-25 09:42:20 +0000

    Refactor : Change class to function
    In main project component, change class grammer to function

--- main/src/component/btn.js
+++ main/src/component/btn.js
@@ -1,31 +1,47 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
-class Selbtn extends Component{
-    constructor(props){
-        super(props)
+const Selbtn = (props)=>{
+    const [clicked, Click] = useState(false);
 
-        this.state = {
-            clicked : false
-        }
-        this.click = this.click.bind(this);
-    }
-    click(){
-        this.setState({
-            clicked : !this.state.clicked
-        })
-    }
+    return(
+        <button 
+            className={clicked? "selBtn clicked" : "selBtn"}
+            onClick={()=>{
+                Click(!clicked); 
+                props.onselected();
+            }}
+        >{props.name}</button>
+    );
 
-    render(){
-        return(
-            <button 
-                className={this.state.clicked? "selBtn clicked" : "selBtn"}
-                onClick={()=>{
-                    this.click(); 
-                    this.props.onselected();
-                }}
-            >{this.props.name}</button>
-        )
-    }
 }
+
+
+// class Selbtn extends Component{
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             clicked : false
+//         }
+//         this.click = this.click.bind(this);
+//     }
+//     click(){
+//         this.setState({
+//             clicked : !this.state.clicked
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <button 
+//                 className={this.state.clicked? "selBtn clicked" : "selBtn"}
+//                 onClick={()=>{
+//                     this.click(); 
+//                     this.props.onselected();
+//                 }}
+//             >{this.props.name}</button>
+//         )
+//     }
+// }
 
 export default Selbtn;
--- main/src/component/select.js
+++ main/src/component/select.js
@@ -1,58 +1,110 @@
-import React, {Component} from "react";
+import React from "react";
 import Selbtn from "./btn";
 
-class Select extends Component{
-    constructor(props){
-        super(props);
-        const btns = [
-            "K-POP(2000-2005)",
-            "K-POP(2005-2010)",
-            "K-POP(2010-2015)",
-            "K-POP(2015-2021)",
-            "2세대 여자아이돌",
-            "3세대 여자아이돌",
-            "4세대 여자아이돌",
-            "롤 스킬 이펙트"
-        ]
-        this.genreBtns = btns.map((genre, idx)=>{
-            return <Selbtn key={idx} name={genre} onselected={()=>{
-                let index = this.selected.genre.indexOf(genre)
-                if(index >-1){this.selected.genre.splice(index,1)}
-                else{this.selected.genre.push(genre)}
-                //console.log(this.selected)
-            }}/>
-        });
+const Select = (props)=>{
 
-        this.selected = {
-            genre : []
-        }
+    //Genres
+    const btns = [
+        "K-POP(2000-2005)",
+        "K-POP(2005-2010)",
+        "K-POP(2010-2015)",
+        "K-POP(2015-2021)",
+        "2세대 여자아이돌",
+        "3세대 여자아이돌",
+        "4세대 여자아이돌",
+        "롤 스킬 이펙트"
+    ]
+
+    const selected = {
+        genre : []
     }
+
+    let genreBtns = btns.map((genre, idx)=>{
+        return <Selbtn key={idx} name={genre} onselected={()=>{
+            let index = selected.genre.indexOf(genre)
+            if(index >-1){selected.genre.splice(index,1)}
+            else{selected.genre.push(genre)}
+            //console.log(this.selected)
+        }}/>
+    });
+
+    return (
+        <div className="selectMode">
+            <div className="selSection">
+                <span>장르</span><br/>
+                {genreBtns}
+            </div>
+            <div className="selSection">
+                <span>방 이름</span><br/>
+                <input className="roomNo" type="text" />
+            </div>
+            <div className="selSection">
+                <span>곡 개수</span><br/>
+                <input className="songNum" type="text" />
+            </div>
+            <div className="selSection">
+                <span>닉네임</span><br/>
+                <input className="nickname" type="text" />
+            </div>
+            <button className="deciBtn" id="accept">확인</button>
+            <button className="deciBtn" onClick={()=>{props.closeWindow()}}>취소</button>
+        </div>
+    );
+}
+
+
+// class Select extends Component{
+//     constructor(props){
+//         super(props);
+//         const btns = [
+//             "K-POP(2000-2005)",
+//             "K-POP(2005-2010)",
+//             "K-POP(2010-2015)",
+//             "K-POP(2015-2021)",
+//             "2세대 여자아이돌",
+//             "3세대 여자아이돌",
+//             "4세대 여자아이돌",
+//             "롤 스킬 이펙트"
+//         ]
+//         this.genreBtns = btns.map((genre, idx)=>{
+//             return <Selbtn key={idx} name={genre} onselected={()=>{
+//                 let index = this.selected.genre.indexOf(genre)
+//                 if(index >-1){this.selected.genre.splice(index,1)}
+//                 else{this.selected.genre.push(genre)}
+//                 //console.log(this.selected)
+//             }}/>
+//         });
+
+//         this.selected = {
+//             genre : []
+//         }
+//     }
     
 
-    render(){
-        return (
-            <div className="selectMode">
-                <div className="selSection">
-                    <span>장르</span><br/>
-                    {this.genreBtns}
-                </div>
-                <div className="selSection">
-                    <span>방 이름</span><br/>
-                    <input className="roomNo" type="text" />
-                </div>
-                <div className="selSection">
-                    <span>곡 개수</span><br/>
-                    <input className="songNum" type="text" />
-                </div>
-                <div className="selSection">
-                    <span>닉네임</span><br/>
-                    <input className="nickname" type="text" />
-                </div>
-                <button className="deciBtn" id="accept">확인</button>
-                <button className="deciBtn" onClick={()=>{this.props.closeWindow()}}>취소</button>
-            </div>
-        );   
-    }   
-}
+//     render(){
+//         return (
+//             <div className="selectMode">
+//                 <div className="selSection">
+//                     <span>장르</span><br/>
+//                     {this.genreBtns}
+//                 </div>
+//                 <div className="selSection">
+//                     <span>방 이름</span><br/>
+//                     <input className="roomNo" type="text" />
+//                 </div>
+//                 <div className="selSection">
+//                     <span>곡 개수</span><br/>
+//                     <input className="songNum" type="text" />
+//                 </div>
+//                 <div className="selSection">
+//                     <span>닉네임</span><br/>
+//                     <input className="nickname" type="text" />
+//                 </div>
+//                 <button className="deciBtn" id="accept">확인</button>
+//                 <button className="deciBtn" onClick={()=>{this.props.closeWindow()}}>취소</button>
+//             </div>
+//         );   
+//     }   
+// }
 
 export default Select;
