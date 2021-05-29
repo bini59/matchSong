baed30fac071d456d83c9ca6ab3430e2a200e86e
유빈 <bean02347@gmail.com>
2021-05-29 13:54:49 +0000

    Refactor : Seperate handler to app

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const io = require("socket.io")
 const RoomRouter = require('./routes/room');
 const chatRouter = require("./routes/chat")
 
+const roomhandler = require("./socketio/connect");
+
 global.room = {
   rooms : []
 }
@@ -53,50 +55,9 @@ app.io = io('', {
   }
 });
 
-
-
-let usercolor = [
-  "red", "blue", "yellow", "green", "purple", "white", "chocolate", "cyan"
-]
   
 app.io.on('connection', (socket)=>{
-  console.log("user connect")
-  socket.on("add-user", (data)=>{
-    let idx = room.rooms.findIndex(i => i.title === data.room.title)
-    let userN = room.rooms[idx].users.length
-    let user = {
-      nickname : data.username,
-      color : usercolor[userN],
-      roomMaster : false,
-      score : 0
-    }
-    if(userN == 0) user.roomMaster = true;
-
-    room.rooms[idx].users.push(user);
-
-    app.io.emit("AddUsr", room.rooms[idx])
-  })
-
-
-  socket.on("remove-user", (data)=>{
-    let idx = room.rooms.findIndex(i => i.title === data.room.title)
-    let usrIdx = room.rooms[idx].users.findIndex(i => i.nickname === data.username)
-    room.rooms[idx].users.splice(usrIdx, 1);
-    if(usrIdx === 0 && room.rooms[idx].users.length > 0){
-      room.rooms[idx].users[0].roomMaster = true;
-    }
-    console.log("disconnected : ", room.rooms[idx].users)
-
-    app.io.emit("remove-user", room.rooms[idx]);
-  })
-  socket.on("disconnect", (data)=>{
-    console.log("disconnect",data);
-  })
-
-  socket.on("send-chat-client", (data)=>{
-    console.log(data);
-    app.io.emit("send-chat-server", data)
-  })
+  roomhandler(app, socket);
 })
 
 module.exports = app;
---
+++ server/socketio/connect.js
@@ -0,0 +1,42 @@
+let usercolor = [
+    "red", "blue", "yellow", "green", "purple", "white", "chocolate", "cyan"
+  ]
+
+module.exports = (app, socket)=>{
+    socket.on("add-user", (data)=>{
+        let idx = room.rooms.findIndex(i => i.title === data.room.title)
+        let userN = room.rooms[idx].users.length
+        let user = {
+            nickname : data.username,
+            color : usercolor[userN],
+            roomMaster : false,
+            score : 0
+        }
+        if(userN == 0) user.roomMaster = true;
+
+        room.rooms[idx].users.push(user);
+
+        app.io.emit("AddUsr", room.rooms[idx])
+    })
+
+
+    socket.on("remove-user", (data)=>{
+        let idx = room.rooms.findIndex(i => i.title === data.room.title)
+        let usrIdx = room.rooms[idx].users.findIndex(i => i.nickname === data.username)
+        room.rooms[idx].users.splice(usrIdx, 1);
+        if(usrIdx === 0 && room.rooms[idx].users.length > 0){
+            room.rooms[idx].users[0].roomMaster = true;
+        }
+        console.log("disconnected : ", room.rooms[idx].users)
+
+        app.io.emit("remove-user", room.rooms[idx]);
+    })
+    socket.on("disconnect", (data)=>{
+        console.log("disconnect",data);
+    })
+
+    socket.on("send-chat-client", (data)=>{
+        console.log(data);
+        app.io.emit("send-chat-server", data)
+    })
+}
