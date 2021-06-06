48471fa2aa39844186a5235eb51699b864052a44
유빈 <bean02347@gmail.com>
2021-06-06 00:47:22 +0000

    Feat : Build react
    React build and accept express

--- server/app.js
+++ server/app.js
@@ -24,11 +24,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/build')));
 
 app.use('/room', cors(),RoomRouter);
 app.use('/chat', chatRouter);
-app.get('/', (req,res)=>{})
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/public/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
